aa21dd65a8153b39534f5bbad6e8b4fd6a73aa45
Saketh Dhulipalla <sakethdhulipalla@sakeths-mbp.devices.brown.edu>
2023-10-26 17:14:26 -0400

    fix: hopefully fixed playwright issue

--- front-end/mock/tests/App.spec.ts
+++ front-end/mock/tests/App.spec.ts
@@ -5,7 +5,7 @@ import { test, expect } from '@playwright/test';
  * This sets up the url for each test
  */
 test.beforeEach(async ({ page }, testInfo) => {
-    await page.goto("http://localhost:8000/");
+  await page.goto("http://localhost:8000/");
   });
 
   /**
